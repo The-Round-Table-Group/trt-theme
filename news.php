40aa092208f8211ff4767eef7d24e92b24f7a67a
Shane Schroll <schroll24@gmail.com>
2024-03-01 00:28:32 -0600

    fixed header for posts/pages/singles, removed staggered delays on menu items (all same delay now) / finished new post page layout and setup

--- news.php
+++ news.php
@@ -4,12 +4,19 @@ $context = Timber::get_context();
 $post = Timber::get_post();
 $context['post'] = $post;
 
+$context['news_tax'] = Timber::get_terms([
+    'taxonomy'   => 'news-tax',
+    'orderby'    => 'name',
+    'order'      => 'DESC',
+    'hide_empty' => false
+]);
+
 $context['news'] = Timber::get_posts([
-    'post_type' => 'news',
+    'post_type'      => 'news',
     'posts_per_page' => -1,
-    'post_status' => 'publish',
-    'orderby' => 'date',
-    'order' => 'DESC'
+    'post_status'    => 'publish',
+    'orderby'        => 'date',
+    'order'          => 'DESC'
 ]);
 
 $templates = ['pages/news.twig'];
