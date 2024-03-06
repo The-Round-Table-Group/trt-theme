ab883685ee3ffbe2d5e9827e0e954556a8677ff9
Shane Schroll <schroll24@gmail.com>
2024-03-06 06:10:56 -0600

    filter styles / layout tweaks and color adjustments

--- clients-and-partners.php
+++ clients-and-partners.php
@@ -4,13 +4,6 @@ $context = Timber::get_context();
 $post = Timber::get_post();
 $context['post'] = $post;
 
-$context['client_tax'] = Timber::get_terms([
-    'taxonomy'   => 'client-tax',
-    'orderby'    => 'name',
-    'order'      => 'DESC',
-    'hide_empty' => false
-]);
-
 $context['clients_and_partners'] = Timber::get_posts([
     'post_type'      => 'client',
     'posts_per_page' => -1,
